c3c306b771da5ae8f18834427625d968326dd4a8
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-05-21 22:51:27 -0400

    passwords

---
+++ webapp_python/data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body' : 'loremipsum',
+            'author' : 'Jung',
+            'create_date': '05-20-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body' : 'loremipsum',
+            'author' : 'Junghoon',
+            'create_date': '05-20-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body' : 'loremipsum',
+            'author' : 'Yoon',
+            'create_date': '05-20-2018'
+        }
+    ]
+
+    return articles
