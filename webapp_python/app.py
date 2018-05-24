827660b9c165300fd5e18390f24ddac1b74115ea
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-05-24 11:31:03 -0400

    finished!

--- webapp_python/app.py
+++ webapp_python/app.py
@@ -52,13 +52,22 @@ def articles():
     cur.close()
 
 
+
 #Single Article
 @app.route('/article/<string:id>/')
 def article(id):
-    return render_template('article.html', id=id)
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get article
+    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+
+    article = cur.fetchone()
+
+    return render_template('article.html', article=article)
 
 
-#Register Form Class
+# Register Form Class
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
@@ -70,8 +79,7 @@ class RegisterForm(Form):
     confirm = PasswordField('Confirm Password')
 
 
-
-#Registration
+# User Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
@@ -95,41 +103,41 @@ def register():
 
         flash('You are now registered and can log in', 'success')
 
-        return redirect(url_for('index'))
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 
-#User Login Information
+# User login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        #Get Form Fields
+        # Get Form Fields
         username = request.form['username']
         password_candidate = request.form['password']
 
-        #Create a Cursor
+        # Create cursor
         cur = mysql.connection.cursor()
 
-        #receiving user info
+        # Get user by username
         result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
 
         if result > 0:
-            # Get matching username and password
+            # Get stored hash
             data = cur.fetchone()
             password = data['password']
 
-        #password comparison
+            # Compare Passwords
             if sha256_crypt.verify(password_candidate, password):
-                #passed
+                # Passed
                 session['logged_in'] = True
                 session['username'] = username
 
                 flash('You are now logged in', 'success')
                 return redirect(url_for('dashboard'))
             else:
-                error = 'INvalid login'
+                error = 'Invalid login'
                 return render_template('login.html', error=error)
-            #Closing connection to DB
+            # Close connection
             cur.close()
         else:
             error = 'Username not found'
@@ -137,7 +145,7 @@ def login():
 
     return render_template('login.html')
 
-#Checking if user has logged in, flask decorator
+# Check if user logged in
 def is_logged_in(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -148,8 +156,7 @@ def is_logged_in(f):
             return redirect(url_for('login'))
     return wrap
 
-
-#Logout
+# Logout
 @app.route('/logout')
 @is_logged_in
 def logout():
@@ -157,8 +164,7 @@ def logout():
     flash('You are now logged out', 'success')
     return redirect(url_for('login'))
 
-
-#Dashboard
+# Dashboard
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
@@ -177,12 +183,13 @@ def dashboard():
         return render_template('dashboard.html', msg=msg)
     # Close connection
     cur.close()
-#Article Form Class
-class ArticleForm(Form):
-   title = StringField('Title', [validators.Length(min=1, max=200)])
-   body = TextAreaField('Body', [validators.Length(min=30)])
 
-#Add Article
+# Article Form Class
+class ArticleForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=200)])
+    body = TextAreaField('Body', [validators.Length(min=30)])
+
+# Add Article
 @app.route('/add_article', methods=['GET', 'POST'])
 @is_logged_in
 def add_article():
@@ -208,6 +215,68 @@ def add_article():
         return redirect(url_for('dashboard'))
 
     return render_template('add_article.html', form=form)
+
+
+# Edit Article
+@app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_article(id):
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get article by id
+    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+
+    article = cur.fetchone()
+    cur.close()
+
+    # Get form
+    form = ArticleForm(request.form)
+
+    # Populate article form fields
+    form.title.data = article['title']
+    form.body.data = article['body']
+
+    if request.method == 'POST' and form.validate():
+        title = request.form['title']
+        body = request.form['body']
+
+        # Create Cursor
+        cur = mysql.connection.cursor()
+        app.logger.info(title)
+        # Execute
+        cur.execute ("UPDATE articles SET title=%s, body=%s WHERE id=%s",(title, body, id))
+        # Commit to DB
+        mysql.connection.commit()
+
+        #Close connection
+        cur.close()
+
+        flash('Article Updated', 'success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('edit_article.html', form=form)
+
+# Delete Article
+@app.route('/delete_article/<string:id>', methods=['POST'])
+@is_logged_in
+def delete_article(id):
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Execute
+    cur.execute("DELETE FROM articles WHERE id = %s", [id])
+
+    # Commit to DB
+    mysql.connection.commit()
+
+    #Close connection
+    cur.close()
+
+    flash('Article Deleted', 'success')
+
+    return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.secret_key='secret123'
